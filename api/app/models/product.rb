a7b2b54079df3f86381a9fa8d3f9181293324ab5
NaofumiOkamoto <n.okamooo@gmail.com>
2023-01-19 13:50:50 +0900

    add Product model

---
+++ api/app/models/product.rb
@@ -0,0 +1,102 @@
+class Product < ApplicationRecord
+
+  # 行の構造を定義
+  Row = Struct.new(
+    :SKU,
+    :artist,
+    :title,
+    :label,
+    :country,
+    :number,
+    :release_year,
+    :recoding_date,
+    :genre,
+    :format,
+    :barcode,
+    :track_list,
+    :personnel,
+    :musical_instrument,
+    :item_condition,
+    :cover_grading,
+    :cover_description,
+    :shopify用mp3_A,
+    :shopify用mp3_B,
+    :yahoo用mp3_A,
+    :yahoo用mp3_B,
+    :record_description_jp,
+    :record_description_en,
+    :record_grading,
+    :weight,
+    :price,
+    :discogs_price,
+    :discogs_id,
+    :ebay_price,
+    :ebay_id,
+    :master_id,
+    :cost_price,
+    :buying_date,
+    :registration_date,
+    :quantity,
+    :youtube,
+    :img_count,
+    :sold_date,
+    :sold_price,
+    :product_status,
+    :sales_status,
+    :ishii_memo
+  )
+
+  def spreadsheets_save(result_values)
+    result_values.drop(1).each do |row_data|
+      row = Row.new(*row_data)
+      attributes = row.to_h.slice(
+        :SKU,
+        :artist,
+        :title,
+        :label,
+        :country,
+        :number,
+        :release_year,
+        :recoding_date,
+        :genre,
+        :format,
+        :barcode,
+        :track_list,
+        :personnel,
+        :musical_instrument,
+        :item_condition,
+        :cover_grading,
+        :cover_description,
+        :shopify用mp3_A,
+        :shopify用mp3_B,
+        :yahoo用mp3_A,
+        :yahoo用mp3_B,
+        :record_description_jp,
+        :record_description_en,
+        :record_grading,
+        :weight,
+        :price,
+        :discogs_price,
+        :discogs_id,
+        :ebay_price,
+        :ebay_id,
+        :master_id,
+        :cost_price,
+        :buying_date,
+        :registration_date,
+        :quantity,
+        :youtube,
+        :img_count,
+        :sold_date,
+        :sold_price,
+        :product_status,
+        :sales_status,
+        :ishii_memo
+      )
+      sku_attributes = row.to_h.slice(:SKU)
+      products = Product.find_or_initialize_by(sku_attributes)
+      products.update(attributes)
+      products.save
+    end
+  end
+end
---
+++ api/db/migrate/20230119002326_create_products.rb
@@ -0,0 +1,50 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+
+      t.integer :SKU
+      t.string :artist
+      t.string :title
+      t.string :label
+      t.string :country
+      t.string :number
+      t.integer :release_year
+      t.string :recoding_date
+      t.string :genre
+      t.string :format
+      t.string :barcode
+      t.string :track_list
+      t.string :personnel
+      t.string :musical_instrument
+      t.string :item_condition
+      t.string :cover_grading
+      t.string :cover_description
+      t.string :shopify用mp3_A
+      t.string :shopify用mp3_B
+      t.string :yahoo用mp3_A
+      t.string :yahoo用mp3_B
+      t.string :record_description_jp
+      t.string :record_description_en
+      t.string :record_grading
+      t.integer :weight
+      t.integer :price
+      t.integer :discogs_price
+      t.integer :discogs_id
+      t.decimal :ebay_price
+      t.string :ebay_id
+      t.integer :master_id
+      t.string :cost_price
+      t.string :buying_date
+      t.string :registration_date
+      t.integer :quantity
+      t.string :youtube
+      t.integer :img_count
+      t.string :sold_date
+      t.integer :sold_price
+      t.string :product_status
+      t.string :sales_status
+      t.string :ishii_memo
+      t.timestamps
+    end
+  end
+end
