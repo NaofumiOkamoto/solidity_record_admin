f2904f33a91736ef5b8f104ae76573b56a189e81
NaofumiOkamoto <n.okamooo@gmail.com>
2023-02-13 09:50:22 +0900

    cors

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
+    origins ['http://localhost:3000', 'http://153.127.17.205']
 
     resource "*",
       headers: :any,
