b455a6bb420ccc245fe91ef234c93b65eceef9fa
NaofumiOkamoto <n.okamooo@gmail.com>
2023-02-14 22:54:22 +0900

    レイアウト修正

--- front/src/page/csv.tsx
+++ front/src/page/csv.tsx
@@ -21,32 +21,57 @@ export const Csv = () => {
     })
   }
 
-  const platforms = [
+  type Platforms = 'discogs' | 'mercari'
+
+  const platforms: Array<Platforms> = [
     'discogs',
     'mercari',
   ]
   const Container = styled.div`
   margin: 2rem;
 `
+  const Button = styled.button`
+    background-color: #FFFFFF;
+    border: 1px solid rgb(209,213,219);
+    border-radius: .5rem;
+    box-sizing: border-box;
+    color: #111827;
+    font-family: "Inter var",ui-sans-serif,system-ui,-apple-system,system-ui,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+    font-size: .875rem;
+    font-weight: 600;
+    line-height: 1.25rem;
+    padding: .75rem 1rem;
+    text-align: center;
+    text-decoration: none #D1D5DB solid;
+    text-decoration-thickness: auto;
+    box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
+    cursor: pointer;
+    user-select: none;
+    -webkit-user-select: none;
+    touch-action: manipulation;
+  `
+  const Hr = styled.hr`
+    width: 90%;
+    background-color: #dddddd;
+    height: 1px;
+    border: none;
+  `
 
   return (
     <>
-      {platforms.map((platform) => {
-        {/* <label for="country-select">国を選択</label>
-        <select name="countries" id="country-select">
-            <option value="">国</option>
-            <option value="dog">japan</option>
-            <option value="cat">海外</option>
-        </select> */}
+      {platforms.map(platform => {
         return (
-          <Container>
-            <h3>{platform}</h3>
-            <button
-              onClick={() => getApi(platform)}
-            >
-              csvを出力
-            </button>
-          </Container>
+          <>
+            <Container>
+              <h3>{platform}</h3>
+              <Button
+                onClick={() => getApi(platform)}
+              >
+                csvを出力
+              </Button>
+            </Container>
+            <Hr />
+          </>
         )
       })}
     </>
