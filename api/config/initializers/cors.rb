5c598a5b1d45faaf5828ccf758be7616995b48b0
NaofumiOkamoto <n.okamooo@gmail.com>
2023-02-14 10:04:02 +0900

    cors

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:3000', 'http://153.127.17.205']
+    origins ['http://localhost:3000', 'http://153.127.17.205', 'http://admin.solidityrecords.com', 'https://admin.solidityrecords.com']
 
     resource "*",
       headers: :any,
