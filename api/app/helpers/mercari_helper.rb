4dadb417c4b5c2ef15e81e2ab9823932fb029366
NaofumiOkamoto <n.okamooo@gmail.com>
2023-03-01 21:54:25 +0900

    画像名0埋め

--- api/app/helpers/mercari_helper.rb
+++ api/app/helpers/mercari_helper.rb
@@ -66,7 +66,7 @@ module MercariHelper
     img_count = value['img_count']
     IMG_COUNT.to_i.times do |i|
       if img_count.to_i > i
-        img_row[i] = "#{value['SKU']}_#{i + 1}.jpg"
+        img_row[i] = "#{value['SKU']}_#{format("%02d", i + 1)}.jpg"
       else
         img_row[i] = ''
       end
